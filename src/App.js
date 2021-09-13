bbdacc63457d47acffcefb7c77e2342302e31950
Dario Navarrete <rd_navarrete@outlook.com>
2021-09-13 12:09:03 -0500

    Creados componentes Footer, Header, Productos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Producto from "./components/Producto"
 
 function App() {
+
+
+  // Crear listado de productos
+  const [productos, guardarProductos] = useState([
+    { id: 1, nombre: 'Camisa ReactJS', precio: 50 },
+    { id: 2, nombre: 'Pantalones AngularJS', precio: 40 },
+    { id: 3, nombre: 'Gorra NodeJS', precio: 10 },
+    { id: 4, nombre: 'Blusa ViuJs', precio: 20 }
+
+  ]);
+
+  // Obtener la fecha actualment
+  const fecha = new Date().getFullYear();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header
+        titulo='Tienda Virtual'
+      />
+
+      <h1>Lista de Productos</h1>
+      {productos.map(producto => (
+        <Producto
+          key={producto.id}
+          producto={producto}
+        />
+      ))}
+
+      <Footer
+        fecha={fecha}
+      />
+    </Fragment>
   );
 }
 
---
+++ src/components/Producto.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Producto = ({ producto }) => {
+
+    const { nombre, precio, id } = producto;
+
+    return (
+        <div>
+            <h2>{nombre}</h2>
+            <p>${precio}</p>
+            <button
+                type="button"
+                onClick={() =>}
+            >Compra</button>
+        </div>
+    );
+}
+
+export default Producto;
